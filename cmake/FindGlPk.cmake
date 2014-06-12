02b5470105f11440526bd2d655c9fb0cce5ad86c
Arie Gurfinkel <arie@cmu.edu>
2014-06-11 22:50:28 -0400

    minor

--- cmake/FindGlPk.cmake
+++ cmake/FindGlPk.cmake
@@ -8,7 +8,7 @@ find_path(GLPK_INCLUDE_DIR NAMES glpk.h PATHS ${GLPK_ROOT}/include)
 find_library(GLPK_LIBRARY NAMES glpk PATHS ${GLPK_ROOT}/lib)
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLPK
+find_package_handle_standard_args(GlPk
   REQUIRED_VARS GLPK_LIBRARY GLPK_INCLUDE_DIR)
 
 mark_as_advanced(GLPK_LIBRARY GLPK_INCLUDE_DIR)
